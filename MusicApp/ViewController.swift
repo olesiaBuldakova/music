8e1c91aecdd1c2504548b5fb4569960a2e43fc1c
olesya buldakova <lesyabuld@gmail.com>
2019-03-17 15:37:07 +0300

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Леся Булдакова on 17.03.2019.
+//  Copyright © 2019 Леся Булдакова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
